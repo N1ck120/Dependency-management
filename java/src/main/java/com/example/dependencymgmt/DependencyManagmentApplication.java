d401bdf656340adc48194c9eceba37ae9b7ea9ca
N1ck120 <nickolasaugusto74@gmail.com>
2024-11-19 19:58:52 -0300

    Commit inicial

---
+++ java/src/main/java/com/example/dependencymgmt/DependencyManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependencymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyManagmentApplication.class, args);
+	}
+
+}
---
+++ java/src/test/java/com/example/dependencymgmt/DependencyManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependencymgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
